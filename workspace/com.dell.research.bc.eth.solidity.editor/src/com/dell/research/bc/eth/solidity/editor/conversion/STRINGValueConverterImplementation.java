eae00fa05c5f0a2ac7249852c53aeb9b3f4a24fb
Daniel Ford <webdaford@fordcochrane.com>
2016-05-15 03:02:32 -0400

    implemented custom terminals for Solidity grammar.
    Fixes problem with parsing integers larger than 2^32-1.

---
+++ workspace/com.dell.research.bc.eth.solidity.editor/src/com/dell/research/bc/eth/solidity/editor/conversion/STRINGValueConverterImplementation.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Keoja LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Daniel Ford, Keoja LLC - initial API and implementation
+ *******************************************************************************/
+
+package com.dell.research.bc.eth.solidity.editor.conversion;
+
+import org.eclipse.xtext.conversion.IValueConverter;
+import org.eclipse.xtext.conversion.ValueConverterException;
+import org.eclipse.xtext.nodemodel.INode;
+/**
+ * see: https://eclipse.org/Xtext/documentation/303_runtime_concepts.html#value-converter 
+ */
+public class STRINGValueConverterImplementation implements IValueConverter<String> {
+
+	@Override
+	public String toValue(String string, INode node) throws ValueConverterException {
+		// Strip leading and trailing quotes.
+		String retValue = string.replaceAll("^\"|\"$", "");
+		return retValue;
+	}
+
+	@Override
+	public String toString(String value) throws ValueConverterException {
+		return value;
+	}
+
+} // STRINGValueConverterImplementation
